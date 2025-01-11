fa07f22df6f8340cf4d9b6acda70ba54a30ace6a
Abdullah Mamun <abdullah.mamun.0110@gmail.com>
2025-01-11 11:47:55 +0600

    group and freighn key also learned

--- basic_curd.sql
+++ basic_curd.sql
@@ -3,28 +3,8 @@
 CREATE DATABASE new_db
 
 -- create table
-CREATE TABLE students (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    age INTEGER NOT NULL,
-    address VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    adulthood_status VARCHAR(50) DEFAULT 'adults'
-);
 
 -- add data on table
-INSERT INTO students (name ,age,address , created_at)
-VALUES 
-    ('Bob', 25, '11 Wall St, CityCenter', DEFAULT),
-    ('Carol', 24, '23 Main St, Suburbiaville', DEFAULT),
-    ('David', 22, '456 Elm St, Townsville', DEFAULT),
-    ('Eve', 27, '789 Oak St, Villagetown', DEFAULT),
-    ('Frank', 29, '123 Main St, Cityville', DEFAULT),
-    ('George', 15, '456 Elm St, Townsville', DEFAULT),
-    ('Helen', 17, '789 Oak St, Villagetown', DEFAULT),    
-    ('Irene', 18, '890 Pine St, Villagetown', DEFAULT),
-    ('Jack', 19, '123 Maple St, Townsville', DEFAULT),
-    ('Kathy', 13, '456 Birch St, Cityville', DEFAULT)
 
 
 -- retrieve all data
