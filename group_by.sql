617964f75aabb9a5b43bc7ea32f7eea90c8a9c5e
Abdullah Mamun <abdullah.mamun.0110@gmail.com>
2025-01-11 11:48:11 +0600

    group and freighn key also learned

--- group_by.sql
+++ group_by.sql
@@ -1,3 +1,13 @@
 -- counting similar age
-SELECT age , count(29) from students
+SELECT age , count(*) from students
     GROUP BY age;
+    
+-- count how many adults and how many teenage or child
+SELECT
+    
+        WHEN age >= 18 
+        ELSE 'Not Adult'
+    END AS status,
+    COUNT(*) AS count
+FROM students
+GROUP BY status;
